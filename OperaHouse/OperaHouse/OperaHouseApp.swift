2c0aabc22db03222db05f77fe8b71e6f5e14832b
Tibin Thomas <tibinmutholy@gmail.com>
2023-09-20 18:30:19 -0400

    project init

---
+++ OperaHouse/OperaHouse/OperaHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperaHouseApp.swift
+//  OperaHouse
+//
+//  Created by Tibin Thomas on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct OperaHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
